5960365e477c8416e2147545cf4e23c4cecf6268
Alex Palmer <xanderx@xanderx.com>
2017-07-09 16:34:37 +0100

    Add support for single-disc releases

--- TagLogsAndCues.py
+++ TagLogsAndCues.py
@@ -24,6 +24,13 @@ def find_disc_number(file):
 
     return None
 
+def read_text_from_file(file, encoding):
+    """Reads text from file with encoding.
+
+    Assumes Windows line endings for compatibility with Foobar2000's tag viewer."""
+    with file.open(encoding=encoding, newline='\r\n') as handle:
+        return handle.read()
+
 def map_disc_numbers_to_file_contents(files, encoding):
     """Maps the disc numbers of files to their contents
 
@@ -33,10 +40,22 @@ def map_disc_numbers_to_file_contents(files, encoding):
     for file in files:
         disc_number = find_disc_number(file)
         if disc_number is not None:
-            with file.open(encoding=encoding, newline='\r\n') as handle:
-                disc_numbers_to_file_contents[disc_number] = handle.read()
+            disc_numbers_to_file_contents[disc_number] = read_text_from_file(file, encoding)
 
     return disc_numbers_to_file_contents
+
+def map_disc_numbers_to_values_map(files, encoding):
+    """Gets a map mapping file disc numbers to appropriates values, usually file contents."""
+    files = list(files)
+    tag_map = map_disc_numbers_to_file_contents(files, encoding)
+
+    if len(list(files)) == 1 and len(tag_map) == 0:
+        # There is only one file and it did not feature a disc number in the name,
+        # therefore assume it is disc 1 of a single-disc release
+        print('Only one file with no obvious disc number, assuming single-disc release')
+        tag_map = {'1': read_text_from_file(files[0], encoding)}
+
+    return tag_map
 
 def apply_disc_specific_tag(path, music_file, disc_mapping, tag):
     """Applies the appropriate disc-specific mapping to music_files's tag, if one exists"""
@@ -87,18 +106,19 @@ def main():
     args = parser.parse_args()
 
     music_path = Path(args.music_path)
+    print(music_path)
     if not music_path.is_dir():
         print('music_path is not a directory or does not exist', file=sys.stderr)
         sys.exit(1)
 
     # Find LOGs
-    disc_numbers_to_logs = map_disc_numbers_to_file_contents(
+    disc_numbers_to_logs = map_disc_numbers_to_values_map(
         music_path.glob('*.log'),
         args.log_encoding
     )
 
     # Find CUEs
-    disc_numbers_to_cues = map_disc_numbers_to_file_contents(
+    disc_numbers_to_cues = map_disc_numbers_to_values_map(
         music_path.glob('*.cue'),
         args.cue_encoding
     )
